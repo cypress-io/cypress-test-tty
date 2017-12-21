99548f3494ee9e08212033ed5ab5a08e5d93f32b
Brian Mann <brian@backbonerails.com>
2017-12-21 13:01:18 -0500

    test color output on stdout and stderr

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const tty = require('tty')
+const chalk = require('chalk')
 
 console.log('stdout is tty?', process.stdout.isTTY, tty.isatty(1))
 console.log('stderr is tty?', process.stderr.isTTY, tty.isatty(2))
+
+console.log('colored output on stdout?', chalk.red('this should be red'))
+console.error('colored output on stderr?', chalk.red('this should be red'))
